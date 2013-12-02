dc385d95356a0d1f5eb24289cdb8545e44686aec
Tomas Bottyan <tesseract@mailbox.hu>
2013-12-03 00:16:51 +0100

    Typo fix & Cleaned old, unused files

--- static/js/hsbp.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-    $(".scroll").click(function(event){     
-        event.preventDefault();
-        $('html,body').animate({scrollTop: $(this.hash).offset().top - $('#nav').height() - 50}, 500);
-    });
-});
-
